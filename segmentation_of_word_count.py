c7f5fffb515e2d40de1ff2c08adf61096d612f2b
wan <39131963+chingchengWan@users.noreply.github.com>
2019-12-30 00:16:14 +0800

    Update segmentation_of_word_count.py

--- segmentation_of_word_count.py
+++ segmentation_of_word_count.py
@@ -1,22 +1,22 @@
 import re
 import sys
 
-def read_file(): # 讀取一個文本，統計文本中單詞的出現次數
+def read_file(): 
     with open('/Users/wan/segmentation_of_word/bodyfile.txt', 'r', encoding='utf-8') as f:
-        word = []  # 空陣列用来儲存文本中的單詞
-        for word_str in f.readlines():  #分行讀取文本，且返回的是一個列表，每行的值當列表中的一個元素：
-            word_str = word_str.replace(',', '') #去除原檔空格
-            word_str = word_str.strip() #strip去除每行字符中的空白字符
-            word_list = re.split('，|。|；|？',word_str)#對當行字符串通過逗號進行分割，回傳list
-            word.extend(word_list) #把分割後的lust内容，加到word陣列裡
+        word = [] 
+        for word_str in f.readlines():  
+            word_str = word_str.replace(',', '') 
+            word_str = word_str.strip() 
+            word_list = re.split('，|。|；|？',word_str)
+            word.extend(word_list) 
         print("Finish readfile() sentence count is {}".format(len(word)))
         return word
  
  
 def clear_account(lists):
-    count_dict = {} #定義一空字典，用來存放分割後的詞和出現次數
+    count_dict = {} 
     # count_dict = count_dict.fromkeys(lists) 
-    # word_list1 = list(count_dict.keys()) #取出字典中的key，放到word_list1
+    # word_list1 = list(count_dict.keys()) 
     word_list1 = lists
     for word in word_list1:
         if word in count_dict :
@@ -24,20 +24,13 @@ def clear_account(lists):
         else :
             count_dict[word] = 1
     return count_dict
-
-"""
-    #計算句子出現的次數,並存入count_dict字典中
-    for i in word_list1:
-        count_dict[i] = lists.count(i)#使用count計算出現次數
-    return count_dict
-"""
  
  
 def sort_dict(count_dict):
     if '' in count_dict:
-        del count_dict[''] #刪除字典中''單字
-    my_dict = sorted(count_dict.items(), key=lambda d:d[1], reverse=True)  # 臨時參數d[1]是用value排序
-    my_dict = dict(my_dict) #轉成字典
+        del count_dict[''] 
+    my_dict = sorted(count_dict.items(), key=lambda d:d[1], reverse=True)
+    my_dict = dict(my_dict) 
     return my_dict
  
  
